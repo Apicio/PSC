7899c490bb01c111f817d41adf6a2cce5a03d196
ggiallo28 <th3g91@gmail.com>
2016-03-16 17:31:50 +0100

    aggiunta cindir.m

--- Regressore_edit/MAIN_InitScript.m
+++ Regressore_edit/MAIN_InitScript.m
@@ -1,6 +1,6 @@
 clc;
 warning off
-StopVrepSimulation(); %pre precauzione
+% StopVrepSimulation(); %pre precauzione
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%   Set Path  %%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -17,13 +17,17 @@ savepath;
 InitConnectionWithSimulator();%initialize the communication with the Simulator
 %MotorParametersBushelessLafert(); %load in workspace the parameters of the motors
 
-load('min_fmincon_21.77.mat');
+% load('min_fmincon_21.77.mat');
 traj = genTraj(min3, [1:1:50], 6, 50, [0:2:98], 10);
-
+points = [];
 for i=1:size(traj,2)
     pause(0.1);
     JOINT = [0 0 traj(1,i) traj(2,i) traj(3,i) traj(4,i) traj(5,i) traj(6,i)];
 	SendPoseToVRep(JOINT);
+    [p, phi, R, A] = cindir(JOINT, 'ZYZ');
+    if(p(3) < 300)
+        points = [points, i];
+    end
 end
 %ESEMPIO DI MOVIMENTAZIONE DEL ROBOT
 %JOINT = [1 1 0 -pi/2 pi/4 0 0 0]; %(m (x), m(y), rad(joint1), rad(joint2), rad(joint3), rad(joint4), rad(joint5), rad(joint6))

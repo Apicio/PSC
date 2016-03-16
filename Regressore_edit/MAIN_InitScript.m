f68801c0770f90685996934dc0baf359c61c7793
ggiallo28 <th3g91@gmail.com>
2016-03-16 23:33:08 +0100

    Aggiunto filtraggio index

--- Regressore_edit/MAIN_InitScript.m
+++ Regressore_edit/MAIN_InitScript.m
@@ -23,25 +23,17 @@ for j=1:size(min3,2)
     [p, phi, R, A] = cindir([0,0,min3(1:6,j)'], 'ZYZ');
     points = [points, p];
 end
-Ix = find(points(1,:)>300);
-Iz = find(points(3,:)>300);
-I = intersect(Ix,Iz);
-minP = min3(:,I);
+nIx = find(points(1,:)<0.3);
+nIz = find(points(3,:)<0.3);
+nI = union(intersect(nIx,nIz), nIz);
+min3(:,nI) = [];
 % minN = [min3(:,3:6), min3(:,9:12), min3(:,14:15), min3(:,17:18), min3(:,20:33), min3(:,38:42), min3(:,44:49)];
-minE = 
-[times, srt] = minPlot(minN);
-traj = genTraj(minN, 1:1:size(minN,2), 6, size(minN,2), times, 1000);
+[times, srt] = minPlot(minP);
+traj = genTraj(minP, 1:1:size(minP,2), 6, size(minP,2), times, 1000);
 points = [];
 for i=1:size(traj,2)
     JOINT = [0 0 traj(1,i) traj(2,i) traj(3,i) traj(4,i) traj(5,i) traj(6,i)];
 	SendPoseToVRep(JOINT);
-end
-for j=1:size(min3,2)
-    [p, phi, R, A] = cindir([0,0,min3(1:6,j)'], 'ZYZ');
-    p(3)
-    if(p(3) < 0.300)
-        points = [points, j];
-    end
 end
 %ESEMPIO DI MOVIMENTAZIONE DEL ROBOT
 %JOINT = [1 1 0 -pi/2 pi/4 0 0 0]; %(m (x), m(y), rad(joint1), rad(joint2), rad(joint3), rad(joint4), rad(joint5), rad(joint6))

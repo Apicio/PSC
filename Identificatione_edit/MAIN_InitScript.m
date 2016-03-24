fee2bf9721d4077f778754b5e9e29bcbd4ab0327
ggiallo28 <th3g91@gmail.com>
2016-03-24 16:49:40 +0100

    Risolto delay

--- Identificatione_edit/MAIN_InitScript.m
+++ Identificatione_edit/MAIN_InitScript.m
@@ -24,6 +24,7 @@ load('traj.mat');
 load('IdMatrix.mat');
 load('min_value.mat');
 distance_from_wall = 0.50;
+delay = 300;
 %% 
 cutoff = 42000;
 t = IdMatrix(cutoff:end,1);        % Vettore dei tempi
@@ -32,25 +33,29 @@ qSim = IdMatrix(cutoff:end,23:29); % Matrice Q, variabili di giunto LETTE dal co
 ISim = IdMatrix(cutoff:end,37:43); % Matrice delle Correnti lette dai sensori
 %% Troviamo i punti della traiettoria
 cycle = size(qInv,1) - size(Traj,1);
-no = [];
-% Et = sum(Traj.^2);
-% for i=1:cycle
-%     Ea = sum(qInv(i:size(Traj,1)+i-1,:).^2);
-%     no = [no, mean(abs(Ea-Et))];
-% end
+no = zeros(1,cycle);
+Et = sum(Traj.^2);
 for i=1:cycle
-    tmp = qInv(i:size(Traj,1)+i-1,:);
-    diff = abs(tmp(:) - Traj(:));
-    no = [no, mean(diff)];
+    Ea = sum(qInv(i:size(Traj,1)+i-1,:).^2);
+    no(i) = mean(abs(Ea-Et));
 end
-i = find(no == min(no));
+i = find(no == min(no));                        %cutoff index
+no = zeros(1,delay);
+Et = sum(qInv(i:size(Traj,1)+i-1,1:6).^2);
+for j=i:delay+i
+    Ea = sum(qSim(j:size(Traj,1)+j-1,1:6).^2);
+    no(j-i+1) = mean(abs(Ea-Et));
+end
+j = find(no == min(no));                        %delay index
 %% Undirted Data
-t = t(i:size(Traj,1)+i-1);           % Vettore dei tempi
-qInv = qInv(i:size(Traj,1)+i-1,:);   % Matrice Q, variabili di giunto INVIATE al controllore
-qSim = qSim(i:size(Traj,1)+i-1,:); % Matrice Q, variabili di giunto LETTE dal controllore
-ISim = ISim(i:size(Traj,1)+i-1,:); % Matrice delle Correnti lette dai sensori
-figure; plot(qInv);
-figure; plot(Traj);
+t = t(i:size(Traj,1)+i-1);                   % Vettore dei tempi
+qInv = qInv(i:size(Traj,1)+i-1,:);           % Matrice Q, variabili di giunto INVIATE al controllore
+qSim = qSim((i+j):size(Traj,1)+(i+j)-1,:);   % Matrice Q, variabili di giunto LETTE dal controllore
+ISim = ISim((i+j):size(Traj,1)+(i+j)-1,:);   % Matrice delle Correnti lette dai sensori
+figure; plot(qInv); title('qInv');
+figure; plot(Traj); title('Traj');
+figure; plot(qSim); title('qSim');
+figure; plot(ISim); title('ISim');
 
 
 

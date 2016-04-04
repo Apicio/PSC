65e511ff7f23512e5281ebb6b42975b10b139cd4
Gianluigi Mucciolo <th3g91@gmail.com>
2016-04-04 21:03:39 +0200

    Aggiunto calcolo matrici

---
+++ Matrici/generateModel.m
@@ -0,0 +1,31 @@
+function [G, Cor, Fs, Fv, B] = generateModel(qDH, dqDH, ddqDH)
+% B(q)ddq + C(q,dq)dq + Fv*dq + sign(dq)*Fs + g(q) = tau
+load('pi.mat')
+G = Regressore(qDH,zeros(1,6),zeros(1,6))*pigreca; % g(q) = tau
+
+B(:,1) =  Regressore(qDH,zeros(1,6),[1,0,0,0,0,0])*pigreca - G; %% B(q)ddq + g(q) = tau quindi tolgo G per ottenere B(q)ddq
+B(:,2) =  Regressore(qDH,zeros(1,6),[0,1,0,0,0,0])*pigreca - G;
+B(:,3) =  Regressore(qDH,zeros(1,6),[0,0,1,0,0,0])*pigreca - G;
+B(:,4) =  Regressore(qDH,zeros(1,6),[0,0,0,1,0,0])*pigreca - G;
+B(:,5) =  Regressore(qDH,zeros(1,6),[0,0,0,0,1,0])*pigreca - G;
+B(:,6) =  Regressore(qDH,zeros(1,6),[0,0,0,0,0,1])*pigreca - G;
+
+Sn(:,1) =  Regressore(qDH,[-1,0,0,0,0,0],zeros(1,6))*pigreca - G; % C(q,dq)dq + Fv*dq - Fs
+Sn(:,2) =  Regressore(qDH,[0,-1,0,0,0,0],zeros(1,6))*pigreca - G;
+Sn(:,3) =  Regressore(qDH,[0,0,-1,0,0,0],zeros(1,6))*pigreca - G;
+Sn(:,4) =  Regressore(qDH,[0,0,0,-1,0,0],zeros(1,6))*pigreca - G;
+Sn(:,5) =  Regressore(qDH,[0,0,0,0,-1,0],zeros(1,6))*pigreca - G;
+Sn(:,6) =  Regressore(qDH,[0,0,0,0,0,-1],zeros(1,6))*pigreca - G;
+
+Sp(:,1) =  Regressore(qDH,[1,0,0,0,0,0],zeros(1,6))*pigreca - G; % C(q,dq)dq + Fv*dq + Fs
+Sp(:,2) =  Regressore(qDH,[0,1,0,0,0,0],zeros(1,6))*pigreca - G;
+Sp(:,3) =  Regressore(qDH,[0,0,1,0,0,0],zeros(1,6))*pigreca - G;
+Sp(:,4) =  Regressore(qDH,[0,0,0,1,0,0],zeros(1,6))*pigreca - G;
+Sp(:,5) =  Regressore(qDH,[0,0,0,0,1,0],zeros(1,6))*pigreca - G;
+Sp(:,6) =  Regressore(qDH,[0,0,0,0,0,1],zeros(1,6))*pigreca - G;
+
+CFv = (Sp+Sn)./2; % C(q,dq)dq + Fv*dq
+Fs = Sp - CFv;    % C(q,dq)dq + Fv*dq + Fs - C(q,dq)dq + Fv*dq = Fs
+
+end
+
--- Regressore_edit/minPlot.m
+++ Regressore_edit/minPlot.m
@@ -12,15 +12,15 @@ function [times, srt] = minPlot(min, F, tstart, tend)
           points(1:3,i) = p;
     end
     figure
-    plot(linspace(times(1),times(end),size(traj,2)),points(3,:));
+    plot(linspace(times(1),times(end),size(traj,2)),points(3,:)-0.5);
     
     points_m = zeros(3,size(min,2));
     for j=1:size(min,2)
         [p, ~, ~] = cindir([0,0,min(1:6,j)'], 'ZYZ');
         points_m(:,j) = p;
         hold on
-        stem(times(j),points_m(3,srt(j)),'r-o');
-        text(times(j),points_m(3,srt(j)),num2str(j-1));
+        stem(times(j),points_m(3,srt(j)) -0.5,'r-o');
+        text(times(j),points_m(3,srt(j)) -0.5,num2str(j-1));
     end 
     title('Z');
     figure

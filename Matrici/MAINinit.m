6df563db475eb29248387686aeb4eafbe96a140d
Gianluigi Mucciolo <th3g91@gmail.com>
2016-04-05 18:47:32 +0200

    niente

--- Matrici/MAINinit.m
+++ Matrici/MAINinit.m
@@ -1,10 +1,5 @@
 clc; clear all; close all;
 % B(q)ddq + C(q,dq)dq + Fv*dq + sign(dq)*Fs(dq) + g(q) = tau
-Regressore([1, zeros(1,5)],zeros(1,6),zeros(1,6))*pigreca
-Regressore([zeros(1,1),1,zeros(1,4)],zeros(1,6),zeros(1,6))*pigreca
-Regressore([zeros(1,2),1,zeros(1,3)],zeros(1,6),zeros(1,6))*pigreca
-Regressore([zeros(1,3),1,zeros(1,2)],zeros(1,6),zeros(1,6))*pigreca
-Regressore([zeros(1,4),1,zeros(1,1)],zeros(1,6),zeros(1,6))*pigreca
-Regressore([zeros(1,5),1],zeros(1,6),zeros(1,6))*pigreca
-
-
+qDH = ones(1,6);
+dqDH = ones(1,6);
+[G, Cordq, Fsdq, Fv, Bc, dB] = generateModel(qDH, dqDH);
--- Matrici/generateModel.m
+++ Matrici/generateModel.m
@@ -1,4 +1,4 @@
-function [G, Cordq, Fsdq, Fv, B] = generateModel(qDH, dqDH)
+function [G, Cordq, Fsdq, Fv, Bc, dB] = generateModel(qDH, dqDH)
 % B(q)ddq + C(q,dq)dq + Fv*dq + sign(dq)*Fs + g(q) = tau
 load('pi.mat')
 G = Regressore(qDH,zeros(1,6),zeros(1,6))*pigreca; % g(q) = tau
@@ -10,6 +10,9 @@ B(:,4) =  Regressore(qDH,zeros(1,6),[0,0,0,1,0,0])*pigreca - G;
 B(:,5) =  Regressore(qDH,zeros(1,6),[0,0,0,0,1,0])*pigreca - G;
 B(:,6) =  Regressore(qDH,zeros(1,6),[0,0,0,0,0,1])*pigreca - G;
 
+Bc = diag([mean(B(:,1)),mean(B(:,2)),mean(B(:,3)),mean(B(:,4)),mean(B(:,5)),mean(B(:,6))]);
+dB = B - Bc;
+
 Fv(:,1) = genFv([1,0,0,0,0,0])*pigreca;
 Fv(:,2) = genFv([0,1,0,0,0,0])*pigreca;
 Fv(:,3) = genFv([0,0,1,0,0,0])*pigreca;
@@ -18,6 +21,6 @@ Fv(:,5) = genFv([0,0,0,0,1,0])*pigreca;
 Fv(:,6) = genFv([0,0,0,0,0,1])*pigreca;
 
 Fsdq = regAttStat(dqDH)*pigreca;
-Cordq =  Regressore(qDH,dqDH,zeros(1,6))*pigreca - G - Fv*dqDH - Fsdq;
+Cordq =  Regressore(qDH,dqDH,zeros(1,6))*pigreca - G - Fv*dqDH' - Fsdq;
 end
 

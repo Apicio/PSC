d07a180ca595166a745c29f3f23aef6891798ce5
Gianluigi Mucciolo <th3g91@gmail.com>
2016-05-05 14:56:55 +0200

    fix

--- ConsegnaUNO/DesignControllersVelocity_6.m
+++ ConsegnaUNO/DesignControllersVelocity_6.m
@@ -5,7 +5,7 @@ cd utilis
 ParametriMotori
 Td = 1/500;
 I = Kr^-1*Bconst*Kr^-1;
-Tm = I*Ra*Kt^-1*Kv^-1;          %Cosante di tempo sistema
+TM = I*Ra*Kt^-1*Kv^-1;          %Cosante di tempo sistema
 KM = Kv^-1;                     %Guadagno sistema
 % syms z w ktv kp ktp km kv ktp
 % eq1 = 2*z/w - ktv/(kp*ktp)
@@ -30,102 +30,106 @@ KCV = diag([(2*W(1,1)*Z(1,1))/(KM(1,1)*KTV(1,1)),...
             (2*W(4,4)*Z(4,4))/(KM(4,4)*KTV(4,4)),...
             (2*W(5,5)*Z(5,5))/(KM(5,5)*KTV(5,5)),...
             (2*W(6,6)*Z(6,6))/(KM(6,6)*KTV(6,6))]);   %Guadagno controllore in velocità
-TCV = diag([Tm(1,1) Tm(2,2) Tm(3,3) Tm(4,4) Tm(5,5) Tm(6,6)]);  %Costante di tempo controllore
+TCV = diag([TM(1,1) TM(2,2) TM(3,3) TM(4,4) TM(5,5) TM(6,6)]);  %Costante di tempo controllore
 XR = KCP*KTP*KCV;
 s = tf('s');
-%% Giunto 1 
-giunto = 1;
-F1 = (KM(giunto,giunto)*KCP(giunto,giunto)*KCV(giunto,giunto)*(1+s*TCV(giunto,giunto)))/(s*s*(1+s*Tm(giunto,giunto)));
-F2 = 1+ s*KTV(giunto,giunto)/(KCP(giunto,giunto)*KTP(giunto,giunto));
-F = F1*KTP(giunto,giunto)*F2;
-zpk(F)
-%rltool(minreal(F))
-i =1;
-H = KTP(i,i)*(1 + (s*KTV(i,i))/(KCP(i,i)*KTP(i,i)));
-W = minreal((1/H)/(1+1/F))
-figure, step(W), title(strcat('giunto',num2str(i)))
+%% Progettazione
+C = (KCP*KCV/s)*(s*TCV+eye(6,6));
+H = KTP*(eye(6,6) + (s*KTV)/(KCP*KTP));
+P = KM/(s*(eye(6,6)+s*TM));
+F = C*P*H;
+W = minreal((H^-1)/(eye(6,6)+F^-1));
 % Digitale
-Fd = F*exp(-s*Td/2)
-figure, margin(Fd), title(strcat('delay giunto',num2str(i)))
-W = minreal((1/H)/(1+1/F))
-figure, step(W), title(strcat('delay_giunto',num2str(i)))
-%% Giunto 2 
-giunto = 2;
-F1 = (KM(giunto,giunto)*KCP(giunto,giunto)*KCV(giunto,giunto)*(1+s*TCV(giunto,giunto)))/(s*s*(1+s*Tm(giunto,giunto)));
-F2 = 1+ s*KTV(giunto,giunto)/(KCP(giunto,giunto)*KTP(giunto,giunto));
-F = F1*KTP(giunto,giunto)*F2;
-zpk(F)
-%rltool(minreal(F))
-i =2;
-H = KTP(i,i)*(1 + (s*KTV(i,i))/(KCP(i,i)*KTP(i,i)));
-W = minreal((1/H)/(1+1/F))
-figure, step(W), title(strcat('giunto',num2str(i)))
-% Digitale
-Fd = F*exp(-s*Td/2)
-figure, margin(Fd), title(strcat('delay giunto',num2str(i)))
-W = minreal((1/H)/(1+1/F))
-figure, step(W), title(strcat('delay_giunto',num2str(i)))
-%% Giunto 3 
-giunto = 3;
-F1 = (KM(giunto,giunto)*KCP(giunto,giunto)*KCV(giunto,giunto)*(1+s*TCV(giunto,giunto)))/(s*s*(1+s*Tm(giunto,giunto)));
-F2 = 1+ s*KTV(giunto,giunto)/(KCP(giunto,giunto)*KTP(giunto,giunto));
-F = F1*KTP(giunto,giunto)*F2;
-zpk(F)
-%rltool(minreal(F))
-i =3;
-H = KTP(i,i)*(1 + (s*KTV(i,i))/(KCP(i,i)*KTP(i,i)));
-W = minreal((1/H)/(1+1/F))
-figure, step(W), title(strcat('giunto',num2str(i)))
-% Digitale
-Fd = F*exp(-s*Td/2)
-figure, margin(Fd), title(strcat('delay giunto',num2str(i)))
-W = minreal((1/H)/(1+1/F))
-figure, step(W), title(strcat('delay_giunto',num2str(i)))
-%% Giunto 4 
-giunto = 4;
-F1 = (KM(giunto,giunto)*KCP(giunto,giunto)*KCV(giunto,giunto)*(1+s*TCV(giunto,giunto)))/(s*s*(1+s*Tm(giunto,giunto)));
-F2 = 1+ s*KTV(giunto,giunto)/(KCP(giunto,giunto)*KTP(giunto,giunto));
-F = F1*KTP(giunto,giunto)*F2;
-zpk(F)
-%rltool(minreal(F))
-i =4;
-H = KTP(i,i)*(1 + (s*KTV(i,i))/(KCP(i,i)*KTP(i,i)));
-W = minreal((1/H)/(1+1/F))
-figure, step(W), title(strcat('giunto',num2str(i)))
-% Digitale
-Fd = F*exp(-s*Td/2)
-figure, margin(Fd), title(strcat('delay giunto',num2str(i))) 
-W = minreal((1/H)/(1+1/F))
-figure, step(W), title(strcat('delay giunto',num2str(i)))
-%% Giunto 5 
-giunto = 5;
-F1 = (KM(giunto,giunto)*KCP(giunto,giunto)*KCV(giunto,giunto)*(1+s*TCV(giunto,giunto)))/(s*s*(1+s*Tm(giunto,giunto)));
-F2 = 1+ s*KTV(giunto,giunto)/(KCP(giunto,giunto)*KTP(giunto,giunto));
-F = F1*KTP(giunto,giunto)*F2;
-zpk(F)
-%rltool(minreal(F))
-i =5;
-H = KTP(i,i)*(1 + (s*KTV(i,i))/(KCP(i,i)*KTP(i,i)));
-W = minreal((1/H)/(1+1/F))
-figure, step(W), title(strcat('giunto',num2str(i)))
-% Digitale
-Fd = F*exp(-s*Td/2)
-figure, margin(Fd), title(strcat('delay giunto',num2str(i)))
-W = minreal((1/H)/(1+1/F))
-figure, step(W), title(strcat('delay giunto',num2str(i)))
-%% Giunto 6 
-giunto = 6;
-F1 = (KM(giunto,giunto)*KCP(giunto,giunto)*KCV(giunto,giunto)*(1+s*TCV(giunto,giunto)))/(s*s*(1+s*Tm(giunto,giunto)));
-F2 = 1+ s*KTV(giunto,giunto)/(KCP(giunto,giunto)*KTP(giunto,giunto));
-F = F1*KTP(giunto,giunto)*F2;
-zpk(F)
-%rltool(minreal(F))
-i =6;
-H = KTP(i,i)*(1 + (s*KTV(i,i))/(KCP(i,i)*KTP(i,i)));
-W = minreal((1/H)/(1+1/F))
-figure, step(W), title(strcat('giunto',num2str(i)))
-% Digitale
-Fd = F*exp(-s*Td/2)
-figure, margin(Fd), title(strcat('delay giunto',num2str(i)))
-W = minreal((1/H)/(1+1/F))
-figure, step(W), title(strcat('delay giunto',num2str(i))) 
+Fd = minreal(F*exp(-s*Td/2));
+Fd = Fd*ones(6,1);
+
+for i=1:6
+    figure, subplot(121), step(W(i,i))
+    Wd(i) = minreal((Fd(i)*H(i,i)^-1)/(1+Fd(i)));
+    subplot(122), step(Wd(i))
+end
+
+i = 6
+rltool(Fd(i))
+
+% 
+% %% Giunto 2 
+% giunto = 2;
+% F1 = (KM(giunto,giunto)*KCP(giunto,giunto)*KCV(giunto,giunto)*(1+s*TCV(giunto,giunto)))/(s*s*(1+s*Tm(giunto,giunto)));
+% F2 = 1+ s*KTV(giunto,giunto)/(KCP(giunto,giunto)*KTP(giunto,giunto));
+% F = F1*KTP(giunto,giunto)*F2;
+% zpk(F)
+% %rltool(minreal(F))
+% i =2;
+% H = KTP(i,i)*(1 + (s*KTV(i,i))/(KCP(i,i)*KTP(i,i)));
+% W = minreal((1/H)/(1+1/F))
+% figure, step(W), title(strcat('giunto',num2str(i)))
+% % Digitale
+% Fd = F*exp(-s*Td/2)
+% figure, margin(Fd), title(strcat('delay giunto',num2str(i)))
+% W = minreal((1/H)/(1+1/F))
+% figure, step(W), title(strcat('delay_giunto',num2str(i)))
+% %% Giunto 3 
+% giunto = 3;
+% F1 = (KM(giunto,giunto)*KCP(giunto,giunto)*KCV(giunto,giunto)*(1+s*TCV(giunto,giunto)))/(s*s*(1+s*Tm(giunto,giunto)));
+% F2 = 1+ s*KTV(giunto,giunto)/(KCP(giunto,giunto)*KTP(giunto,giunto));
+% F = F1*KTP(giunto,giunto)*F2;
+% zpk(F)
+% %rltool(minreal(F))
+% i =3;
+% H = KTP(i,i)*(1 + (s*KTV(i,i))/(KCP(i,i)*KTP(i,i)));
+% W = minreal((1/H)/(1+1/F))
+% figure, step(W), title(strcat('giunto',num2str(i)))
+% % Digitale
+% Fd = F*exp(-s*Td/2)
+% figure, margin(Fd), title(strcat('delay giunto',num2str(i)))
+% W = minreal((1/H)/(1+1/F))
+% figure, step(W), title(strcat('delay_giunto',num2str(i)))
+% %% Giunto 4 
+% giunto = 4;
+% F1 = (KM(giunto,giunto)*KCP(giunto,giunto)*KCV(giunto,giunto)*(1+s*TCV(giunto,giunto)))/(s*s*(1+s*Tm(giunto,giunto)));
+% F2 = 1+ s*KTV(giunto,giunto)/(KCP(giunto,giunto)*KTP(giunto,giunto));
+% F = F1*KTP(giunto,giunto)*F2;
+% zpk(F)
+% %rltool(minreal(F))
+% i =4;
+% H = KTP(i,i)*(1 + (s*KTV(i,i))/(KCP(i,i)*KTP(i,i)));
+% W = minreal((1/H)/(1+1/F))
+% figure, step(W), title(strcat('giunto',num2str(i)))
+% % Digitale
+% Fd = F*exp(-s*Td/2)
+% figure, margin(Fd), title(strcat('delay giunto',num2str(i))) 
+% W = minreal((1/H)/(1+1/F))
+% figure, step(W), title(strcat('delay giunto',num2str(i)))
+% %% Giunto 5 
+% giunto = 5;
+% F1 = (KM(giunto,giunto)*KCP(giunto,giunto)*KCV(giunto,giunto)*(1+s*TCV(giunto,giunto)))/(s*s*(1+s*Tm(giunto,giunto)));
+% F2 = 1+ s*KTV(giunto,giunto)/(KCP(giunto,giunto)*KTP(giunto,giunto));
+% F = F1*KTP(giunto,giunto)*F2;
+% zpk(F)
+% %rltool(minreal(F))
+% i =5;
+% H = KTP(i,i)*(1 + (s*KTV(i,i))/(KCP(i,i)*KTP(i,i)));
+% W = minreal((1/H)/(1+1/F))
+% figure, step(W), title(strcat('giunto',num2str(i)))
+% % Digitale
+% Fd = F*exp(-s*Td/2)
+% figure, margin(Fd), title(strcat('delay giunto',num2str(i)))
+% W = minreal((1/H)/(1+1/F))
+% figure, step(W), title(strcat('delay giunto',num2str(i)))
+% %% Giunto 6 
+% giunto = 6;
+% F1 = (KM(giunto,giunto)*KCP(giunto,giunto)*KCV(giunto,giunto)*(1+s*TCV(giunto,giunto)))/(s*s*(1+s*Tm(giunto,giunto)));
+% F2 = 1+ s*KTV(giunto,giunto)/(KCP(giunto,giunto)*KTP(giunto,giunto));
+% F = F1*KTP(giunto,giunto)*F2;
+% zpk(F)
+% %rltool(minreal(F))
+% i =6;
+% H = KTP(i,i)*(1 + (s*KTV(i,i))/(KCP(i,i)*KTP(i,i)));
+% W = minreal((1/H)/(1+1/F))
+% figure, step(W), title(strcat('giunto',num2str(i)))
+% % Digitale
+% Fd = F*exp(-s*Td/2)
+% figure, margin(Fd), title(strcat('delay giunto',num2str(i)))
+% W = minreal((1/H)/(1+1/F))
+% figure, step(W), title(strcat('delay giunto',num2str(i))) 

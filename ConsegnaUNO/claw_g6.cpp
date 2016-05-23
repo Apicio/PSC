3debecac8fe9765b346ee608fdf95a70d1f8c15c
Gianluigi Mucciolo <th3g91@gmail.com>
2016-05-23 21:47:39 +0200

    implementazione claw in matlab

--- ConsegnaUNO/claw_g6.cpp
+++ ConsegnaUNO/claw_g6.cpp
@@ -1,28 +1,24 @@
 double claw_g6(double qd, double qc){ 
 /* Declaration */
-     static double u;
-     static double uOld;
-     static double uOldOld;
-     static double eOldOld;
-     static double eOld;
-     static double e;
-     static int init;
-/* Inizializzazione */
-     if(init){
-        uOldOld = 0;
-        uOld = 0;
-        u = 0;
-        eOldOld = 0;
-        eOld = 0;
-        e = 0;
-        init = 0;
-     }
+     static double u = 0;
+     static double uOld = 0;
+     static double uOldOld = 0;
+     static double eOldOld = 0;
+     static double eOld = 0;
+     static double e = 0;
 /* Control Law */    
      uOldOld = uOld;
      uOld = u;
      eOldOld = eOld;
      eOld = e;
      e = qd-qc;
-     u = 1.549*uOld - 0.5488*uOldOld + 600*e + 3244*eOld - 3260*eOldOld;
+//   9969 z^2 - 1.686e04 z + 7130      U(z)
+//   ----------------------------  =  -----
+//      z^2 - 1.818 z + 0.8182         E(z)
+//   U(z)(z^2 - 1.818 z + 0.8182) = E(z)(9969 z^2 - 1.686e04 z + 7130)
+//   U(z)*z^2 = U(z)(1.818*z - 0.8182) + E(z)(9969 z^2 - 1.686e04 z + 7130)
+//   u(k+2) = 1.818*u(k+1) - 0.8182*u(k) + 9969*e(k+2) - 16860*e(k+1) + 7130*e(k)
+//   u(k) = 1.818*u(k-1) - 0.8182*u(k-2) + 9969*e(k) - 16860*e(k-1) + 7130*e(k-2)
+     u = 1.818*uOld - 0.8182*uOldOld + 9969*e - 16860*eOld + 7130*eOldOld
      return u;
 }
